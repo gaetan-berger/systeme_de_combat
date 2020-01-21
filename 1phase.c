3ca804c2822d4bbd0378c6a0a9df927fac271b91
gaetan-berger <59962658+gaetan-berger@users.noreply.github.com>
2020-01-21 21:54:42 +0100

    exo3fini

--- 1phase.c
+++ 1phase.c
@@ -6,20 +6,40 @@
 
 int main(void){
 
+int viePerso ;
+int attaquePerso ;
 int vieMonstre ;
-int attaque ;
+int attaqueMonstre ;
 
-attaque = 2 ;
+attaquePerso = 2 ;
+attaqueMonstre = 4 ;
 vieMonstre = 10 ;
+viePerso = 20 ;
+
+printf("un zombie sort de terre il as %d point de vie \n",vieMonstre );
 
 while ( vieMonstre > 0 ){
-printf("un zombie sort de terre il as %d point de vie \n",vieMonstre );
+
 printf("vue ca vitesse tu peux facilement le voir venir et tu l'attaque \n" );
-vieMonstre = vieMonstre - attaque ;
- printf("il reste %d point de vie au zombie\n",vieMonstre );
-}
-if (vieMonstre <= 0){
+vieMonstre = vieMonstre - attaquePerso ;
+if (vieMonstre <= 0 ){
   printf("Bravo tu viens de tuer le zombie \n" );
+  break;
 }
+printf("il reste %d point de vie au zombie\n",vieMonstre );
+printf("attention le zombie contre attaque \n" );
+viePerso = viePerso - attaqueMonstre ;
+if  (viePerso <=0){
+  printf("quelle dommage tu es mort !\n" );
+  break;
+}
+printf("il te reste %d point de vie \n",viePerso );
+
+if (vieMonstre <= 0 && viePerso <= 0){
+  printf("dans un dernier elan tu lui assène le coup fatale\n" );
+  break;
+}
+}
+
     return 0;
 }
