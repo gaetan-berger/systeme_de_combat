5ad54b0269570834efbcb971042f26472354be2c
gaetan-berger <59962658+gaetan-berger@users.noreply.github.com>
2020-01-21 22:16:53 +0100

    exo4fini

--- 1phase.c
+++ 1phase.c
@@ -10,6 +10,7 @@ int viePerso ;
 int attaquePerso ;
 int vieMonstre ;
 int attaqueMonstre ;
+int choix;
 
 attaquePerso = 2 ;
 attaqueMonstre = 4 ;
@@ -17,17 +18,27 @@ vieMonstre = 10 ;
 viePerso = 20 ;
 
 printf("un zombie sort de terre il as %d point de vie \n",vieMonstre );
-
+printf("et toi tu as %d point de vie \n",viePerso );
 while ( vieMonstre > 0 ){
+printf("tu peux l'attaquer = 1 ou te defendre = 2 \n" );
+scanf("%d \n",&choix);
 
+if (choix == 1 ){
 printf("vue ca vitesse tu peux facilement le voir venir et tu l'attaque \n" );
 vieMonstre = vieMonstre - attaquePerso ;
+printf("il reste %d point de vie au zombie\n",vieMonstre );
 if (vieMonstre <= 0 ){
   printf("Bravo tu viens de tuer le zombie \n" );
   break;
 }
-printf("il reste %d point de vie au zombie\n",vieMonstre );
-printf("attention le zombie contre attaque \n" );
+}
+if (choix == 2 ){
+printf("tu te protege de ton bouclier \n" );
+attaqueMonstre = 4 ; // je sias pas si j'ai bon mais si je ne met pas a nouveau lattaque a 4 elle vas etre 4/4 pour 4/4/4 donc le monstre ne pourras pas gagner
+attaqueMonstre = attaqueMonstre / 4;
+}
+
+printf("attention le zombie t'attaque \n" );
 viePerso = viePerso - attaqueMonstre ;
 if  (viePerso <=0){
   printf("quelle dommage tu es mort !\n" );
